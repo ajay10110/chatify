4e1b55696f478018a5f269df6443eeb80df383f5
ajay10110 <ajay.101105@gmail.com>
2026-07-24 00:48:33 +0530

    Welcome Email implemeted

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose"
-
+import { ENV } from "./env.js";
 export const connectDB = async() => {
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(ENV.MONGO_URI)
         console.log("mongodb connected: ", conn.connection.host)
     } catch(error){
         console.log("Error connection to Mongodb",error)
