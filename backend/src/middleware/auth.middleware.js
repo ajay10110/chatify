c818a264b227dd35fb91e9f1f10890496ddc9713
ajay10110 <ajay.101105@gmail.com>
2026-07-24 17:46:56 +0530

    update profile

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import {ENV}from "../lib/env.js";
+import User from "../models/User.js";
+
+export const protectRoute = async (req,res,next) => {
+    try{
+        const token = req.cookies.jwt
+        if(!token) return res.status(401).json({message: "Unauthorized - No token provided"})
+
+            const decoded = jwt.verify(token, ENV.JWT_SECRET)
+            if(!decoded) return resizeBy.status(401).json({message: "Unauthorized - INVALID token"})
+
+            const user = await User.findById(decoded.userId).select("-password")
+            if(!user) return res.status(404).json({message:"User not found"});
+
+            req.user = user ;
+            next();
+    }catch (error) {
+        console.log("Error in protecting middleware" , error);
+        res.status(500).json({message:"Internal server error"});
+    }
+}
