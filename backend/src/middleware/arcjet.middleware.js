b329b97e06afe42902425855e2882dbc90572c0f
ajay10110 <ajay.101105@gmail.com>
2026-07-25 22:12:49 +0530

    Temporarily bypass Arcjet

--- backend/src/middleware/arcjet.middleware.js
+++ backend/src/middleware/arcjet.middleware.js
@@ -1,32 +1,4 @@
-import aj from "../lib/arcjet.js";
-import { isSpoofedBot } from "@arcjet/inspect";
-
 export const arcjetProtection = async (req, res, next) => {
-    try{
-        const decision = await aj.protect(req)
-
-        if(decision.isDenied()){
-            if(decision.reason.isRateLimit()){
-                return res.status(429).json({message: "Rate limimted exceeded. Please try again later"});
-            }
-            else if(decision.reason.isBot()){
-                return res.status(403).json({
-                    message: "BOT_BLOCKED_BY_MY_MIDDLEWARE"
-                });
-            } else{
-                return res.status(403).json({message: "Acess denied by security policy"});
-            }
-        }
-
-        if (decision.results.some(isSpoofedBot)) {
-            return res.status(403).json({
-                error: "Spoofed bot detected",
-                message: "Malicious bot activity detected.",
-            });
-        }
-        next();
-    } catch (error) {
-        console.log("Arcjet Protection Error:" , error);
-        next();
-    }
-}
+  console.log("Arcjet middleware bypassed");
+  next();
+};
